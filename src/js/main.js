03629af7c0150b53fd36a5ee1792dbb777f6de27
LDR <89.freedom@gmail.com>
2023-11-10 15:23:26 +0600

    added several methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+import $ from "./lib/lib";
+
+$("button").on("click", function () {
+    $("div").eq(1).toggleClass("active");
+});
+
+$("div").click(function () {
+    console.log($(this).index());
+});
+
+// console.log($("button").html("hello"));
